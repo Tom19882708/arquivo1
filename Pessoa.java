8a7fd43a87c14a4639b15774da3951d29ba03085
Tom19882708 <eltonvalenssa@gmail.com>
2024-07-02 09:25:30 -0300

    Add files via upload

---
+++ Pessoa.java
@@ -0,0 +1,107 @@
+public class Pessoa main(string [] args);
+ Pessoa p = new Pessoa ();
+ Pessoa p1 = new Pessoa ();
+
+    p.nome = "Messi";
+    p.sobrenome = "andreas lionel"
+    p.idade = "37";
+    p.renda = "400000";
+    p.nacionalidade = "argentina";
+    p.endereco = "rua da lenda expert";
+    p.rg = 987654321 = "0f";
+    p.peso = "71;";
+    p.altura = "1.72";
+    p.time = "inter miami";
+
+    String nome;
+    int idade;
+    double renda;
+    String sobrenome;
+    Float rg;
+    String nacionalidade;
+    String endereco;
+    Float peso;
+    Float altura;
+    String time;
+
+    System.out.println("nome: " +p.nome);
+    System.out.println(" idade: " +p.idade);
+    System.out.println("renda: " +p.renda);
+    System.out.println("sobrenome: " +p.sobrenome);
+    System.out.println("rg: " +p.rg);
+    System.out.println("nacionalidade: " +p.nacionalidade);
+    System.out.println("endereco: " +p.endereco);
+    System.out.println("peso: " +p.peso);
+    System.out.println("altura: " +p.altura);
+    System.out.println("time: " +p.time);
+
+    p.nome = "cristiano ronaldo";
+    p.sobrenome = "dos santos aveiro"
+    p.idade = "39";
+    p.renda = "500000";
+    p.nacionalidade = "portugal";
+    p.endereco = "rua do mister champions";
+    p.rg = 123456789 = "1f";
+    p.peso = "85;";
+    p.altura = "1.81";
+    p.time = "all nars";
+
+    String nome;
+    int idade;
+    double renda;
+    String sobrenome;
+    Float rg;
+    String nacionalidade;
+    String endereco;
+    Float peso;
+    Float altura;
+    String time;
+
+    System.out.println("nome: " +p.nome);
+    System.out.println(" idade: " +p.idade);
+    System.out.println("renda: " +p.renda);
+    System.out.println("sobrenome: " +p.sobrenome);
+    System.out.println("rg: " +p.rg);
+    System.out.println("nacionalidade: " +p.nacionalidade);
+    System.out.println("endereco: " +p.endereco);
+    System.out.println("peso: " +p.peso);
+    System.out.println("altura: " +p.altura);
+    System.out.println("time: " +p.time);
+
+    p.nome = "neymar jr";
+    p.sobrenome = "da silva santos"
+    p.idade = "32";
+    p.renda = "100000";
+    p.nacionalidade = "brasil";
+    p.endereco = "rua do jogador raiz";
+    p.rg = 012345678 = "9f";
+    p.peso = "71;";
+    p.altura = "1.72";
+    p.time = "inter miami";
+
+    String nome;
+    int idade;
+    double renda;
+    String sobrenome;
+    Float rg;
+    String nacionalidade;
+    String endereco;
+    Float peso;
+    Float altura;
+    String time;
+
+    System.out.println("nome: " +p.nome);
+    System.out.println(" idade: " +p.idade);
+    System.out.println("renda: " +p.renda);
+    System.out.println("sobrenome: " +p.sobrenome);
+    System.out.println("rg: " +p.rg);
+    System.out.println("nacionalidade: " +p.nacionalidade);
+    System.out.println("endereco: " +p.endereco);
+    System.out.println("peso: " +p.peso);
+    System.out.println("altura: " +p.altura);
+    System.out.println("time: " +p.time);
+
+
+
+
+    }
